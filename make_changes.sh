0f79ac2353dcc956ec5aea8f54af77d9ee0e128b
Frederik B <fbraun+gh@mozilla.com>
2014-09-29 21:28:37 +0200

    *** for easier log reading, maybe

--- make_changes.sh
+++ make_changes.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "Waiting for device.." &&
+echo *** Waiting for device.. &&
 adb wait-for-device &&
-echo "Let's go." &&
+echo "*** Let's go." &&
 adb remount &&
-echo "Stopping B2G" &&
+echo "*** Stopping B2G" &&
 adb shell stop b2g &&
-echo "Pushing hosts file" &&
+echo "*** Pushing hosts file" &&
 adb push adb_hosts.txt /system/etc/hosts &&
-echo "pulling settings app" &&
+echo "*** pulling settings app" &&
 adb pull /system/b2g/webapps/settings.gaiamobile.org/application.zip &&
-echo "Adding ddg as search provider" &&
+echo "*** Adding ddg as search provider" &&
 zip --verbose -r application.zip resources/search/* &&
 adb push application.zip /system/b2g/webapps/settings.gaiamobile.org/application.zip &&
 rm application.zip &&
-echo "Rebooting" &&
+echo "*** Rebooting" &&
 adb reboot
